19b61ce2364f52146de70978bd80665c1de89531
lucaspq2010@hotmail.com <lucaspq2010@hotmail.com>
2023-05-25 20:27:23 +0000

    primeiros comandos sql

---
+++ Sql01/Ola Mundo.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+SELECT 'OLA MUNDO!'
+
+-- COMMAND ----------
+
+SELECT * --SELECIONE TODAS AS COLUNAS
+FROM silver.pizza_query.pedido --DA TABELA silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+
